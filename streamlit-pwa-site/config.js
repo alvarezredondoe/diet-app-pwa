23e6941602f7b8dcf041fb7133c619ea0baca035
alvarezredondoe <alvarezredondoe@gmail.com>
2026-04-30 17:12:27 +0200

    Update config.js

--- streamlit-pwa-site/config.js
+++ streamlit-pwa-site/config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     appShortName: "MyApp",
     
     // Iframe Config
-    iframeUrl: "https://doc-hello.streamlit.app?embed=true&show_footer=false",
+    iframeUrl: "https://diet-mobile-app-paramo.streamlit.app/#total?embed=true",
     
     // Visuals & Icons
     themeColor: "#0E1117",
